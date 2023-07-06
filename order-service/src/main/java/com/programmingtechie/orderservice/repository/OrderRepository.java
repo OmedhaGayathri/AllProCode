d36c0b73beec698df41fe62c585eebe6ec68ced3
Hashinika Adhikari <67773789+hashinika99@users.noreply.github.com>
2023-07-06 23:30:30 +0530

    Update OrderRepository.java

--- order-service/src/main/java/com/programmingtechie/orderservice/repository/OrderRepository.java
+++ order-service/src/main/java/com/programmingtechie/orderservice/repository/OrderRepository.java
@@ -1,7 +1,1 @@
-package com.programmingtechie.orderservice.repository;
-
-import com.programmingtechie.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
+//code
